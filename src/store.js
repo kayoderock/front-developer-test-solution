4a4afd3158ac1f96da8c8ab846eccc3755d95ca4
kayoderock <kayoderock@gmail.com>
2019-04-08 02:47:36 +0100

    setting the vuex states and installing axios for making nice http requests

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    payments: []
   },
+
   mutations: {
-
+    SET_PAYMENTS (state, payload) {
+      state.payments = payload;
+    }
   },
-  actions: {
 
+  actions: {
+    getAdminDetails({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios.get(API.payemts)
+          .then((response) => {
+            commit('SET_PAYMENTS', response)
+            resolve(response);
+          }).catch((error) => {
+            reject(error);
+          });
+      });
+    },
   }
 })
