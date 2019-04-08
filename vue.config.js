2048c49e9dfc4eada93b8294de8e009ad7def2f0
kayoderock <kayoderock@gmail.com>
2019-04-08 02:35:56 +0100

    setting up my scss

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        sass: {
+          data: `
+          @import "@/sass/main.scss";
+          `,
+        },
+      },
+    },
+  };
+  
