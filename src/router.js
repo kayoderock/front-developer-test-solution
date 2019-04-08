093061381f01b20b041e3bfe3b74fd042d5c6e13
kayoderock <kayoderock@gmail.com>
2019-04-08 02:31:06 +0100

    Adding the vue element component libary and setting the app structure

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+import Container from '../src/views/Containers'
 
 Vue.use(Router)
 
@@ -10,8 +11,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'container',
+      component: Container,
+      children: [
+      ]
     },
     {
       path: '/about',
